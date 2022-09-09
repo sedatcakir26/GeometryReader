48eb752552f7801fbd56f0a18f2db2c583a4dc01
sedatcakir26 <sedatcakir@gmail.com>
2022-09-09 14:29:42 +0300

    Initial Commit

---
+++ GeometryReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReader
+//
+//  Created by Sedat Çakır on 9.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Sedat Çakır on 9.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
